7fe04144944ce4f0a22c6e85cae84b75458fc702
izzygld <izzygld@gmail.com>
2016-11-10 17:03:23 +0200

    search box

--- newcastle/themorningstrut/public_html/includes/authors.php
+++
@@ -1,1 +0,0 @@
-[{"slug":"christopher-t-mcgowan","user_name":"Christopher T. McGowan","bio":""},{"slug":"anna-j-tuttle","user_name":"Anna J. Tuttle","bio":""}]
--- newcastle/themorningstrut/public_html/includes/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// sheket is zahav
-?>
