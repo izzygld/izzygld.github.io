f27e3b10295e372117915ba1ac643167527bbfa6
izzygld <izzygld@gmail.com>
2016-11-10 17:25:19 +0200

    more js

--- searchbox/js/index.js
+++ searchbox/js/index.js
@@ -1,6 +1,21 @@
+
+
+// var btn = document.querySelectorAll(".searchTerm"),
+//     div = document.querySelector("expanded collapsed");
+
+
+//     btn[0].addEventListener('click', function () {
+
+//     myText.textContent = " Koalas are the best animals ";
+
+// });
+
+    $(document).ready(function() {
+
 $(function(){
-  $('.searchTerm').on("focus blur", function(){   
+  $('.searchTerm').on("focus blur", function(){
     $(this).parent().toggleClass("expanded collapsed");
      $(this).siblings('.suggestionBox').toggle();
     });
+});
 });
